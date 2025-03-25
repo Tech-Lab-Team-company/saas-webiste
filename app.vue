29bcfb14f3d70b9b64458006f78103fdd1616aa9
MohabMohamed72 <mohabmohamedd72@gmail.com>
2025-03-25 16:19:31 +0200

    add header and footer

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <MyLayoutHeader></MyLayoutHeader>
+    <MyLayoutFooter></MyLayoutFooter>
   </div>
 </template>
+
+
+
+<style></style>
