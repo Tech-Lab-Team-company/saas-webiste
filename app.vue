7841a3b609408413d62c08e1ff01c122d72a1482
mohamednasra12 <mohamednasra638@gmail.com>
2025-04-07 12:43:10 +0200

    update

--- app.vue
+++ app.vue
@@ -14,20 +14,15 @@ import CourseDetails from "~/components/CourseDetails/CourseDetails.vue";
 <template>
   <div>
     <NuxtLayout>
-<<<<<<< HEAD
 
-=======
+
       <Exam />
       <!-- <CourseDetailsCourseExams /> -->
->>>>>>> 7dc287f9bda3dd3b3a6a8b3936834e575630d847
       <!-- <CourseDetails /> -->
       <!-- <homeStarting /> -->
       <!-- <NuxtPage /> -->
       <!-- <CourseCard /> -->
-<<<<<<< HEAD
-=======
 
->>>>>>> 7dc287f9bda3dd3b3a6a8b3936834e575630d847
 
       <!-- <homeStarting /> -->
       <NuxtPage />
