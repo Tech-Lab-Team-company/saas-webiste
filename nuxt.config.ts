2532b49d988d276c9d81eadb284c0a04911bb33c
rawanmamdooh22 <rawanmamdooh226@gmail.com>
2025-04-23 01:58:14 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,8 +19,8 @@ export default defineNuxtConfig({
     }
   },
   css: [
-    // "@/assets/style/main.min.css",
-    "@/assets/style/main.css",
+    "@/assets/style/main.min.css",
+    // "@/assets/style/main.css",
 
   ],
 })
