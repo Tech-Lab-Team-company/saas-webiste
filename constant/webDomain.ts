a8cab5978282cd91e218abf4e76f4e5862122927
AhmedHawam9 <ahmed.hawam2030@gmail.com>
2025-06-19 12:36:14 +0300

    change header handler

--- constant/webDomain.ts
+++ constant/webDomain.ts
@@ -1,7 +1,9 @@
 export function getWebDomain(): string {
     const url = window.location.href
     try {
+        console.log("webDomain", url);
         const hostname = new URL(url).hostname;
+        console.log("webDomain hostname", hostname);
         if (hostname === 'localhost') return 'hrarabians.org';
         const parts = hostname.split('.');
         return parts.slice(-2).join('.');
