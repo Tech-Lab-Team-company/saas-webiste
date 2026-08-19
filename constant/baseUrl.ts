7af75404bd1f887f7ea96b0e4635d9703ab8a44a
Mohamed Abdelmoneam <m.abdelmoneam.dev@gmail.com>
2026-08-19 15:12:54 +0300

    fix .env

--- constant/baseUrl.ts
+++ constant/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseUrl = "https://dev.saas.techlabeg.com/api/website";
-export const DashboradbaseUrl = "https://dev.saas.techlabeg.com/api/dashboard";
+// export const baseUrl = "https://dev.saas.techlabeg.com/api/website";
+// export const DashboradbaseUrl = "https://dev.saas.techlabeg.com/api/dashboard";
